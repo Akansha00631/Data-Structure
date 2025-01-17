8571301e92392be702d9e992f3910faf05b8811a
AKANSHASINGH_MCA_A_2024-26__04_UEMK <140613636+Akansha00631@users.noreply.github.com>
2025-01-18 01:53:13 +0530

    infix.c

--- infix.c
+++ infix.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
-
-// Stack structure
 #define MAX 100
 char stack[MAX];
 int top = -1;
-
-// Function to push an element onto the stack
 void push(char c) {
     if (top == MAX - 1) {
         printf("Stack Overflow\n");
@@ -15,8 +11,6 @@ void push(char c) {
     }
     stack[++top] = c;
 }
-
-// Function to pop an element from the stack
 char pop() {
     if (top == -1) {
         printf("Stack Underflow\n");
@@ -24,45 +18,40 @@ char pop() {
     }
     return stack[top--];
 }
-
-// Function to return precedence of operators
 int precedence(char op) {
     if (op == '+' || op == '-') return 1;
     if (op == '*' || op == '/') return 2;
     return 0;
 }
-
-// Function to convert infix to postfix
 void infixToPostfix(char* infix, char* postfix) {
     int i = 0, j = 0;
     char c;
     
     while ((c = infix[i++]) != '\0') {
         if (isdigit(c)) {
-            postfix[j++] = c; // Add operands to postfix expression
+            postfix[j++] = c; 
         } else if (c == '(') {
-            push(c); // Push '(' onto the stack
+            push(c); 
         } else if (c == ')') {
             while (top != -1 && stack[top] != '(') {
-                postfix[j++] = pop(); // Pop until '(' is found
+                postfix[j++] = pop(); 
             }
-            pop(); // Remove '(' from the stack
-        } else { // Operator
+            pop(); 
+        } else { 
             while (top != -1 && precedence(stack[top]) >= precedence(c)) {
-                postfix[j++] = pop(); // Pop operators with higher precedence
+                postfix[j++] = pop(); 
             }
-            push(c); // Push the current operator
+            push(c);
         }
     }
     
-    while (top != -1) { // Pop remaining operators
+    while (top != -1) { 
         postfix[j++] = pop();
     }
     
-    postfix[j] = '\0'; // Null-terminate the postfix expression
+    postfix[j] = '\0'; 
 }
 
-// Function to evaluate a postfix expression
 int evaluatePostfix(char* postfix) {
     int stack[MAX];
     int top = -1, i = 0;
@@ -70,8 +59,8 @@ int evaluatePostfix(char* postfix) {
     
     while ((c = postfix[i++]) != '\0') {
         if (isdigit(c)) {
-            stack[++top] = c - '0'; // Convert character to integer and push
-        } else { // Operator
+            stack[++top] = c - '0'; 
+        } else { 
             int b = stack[top--];
             int a = stack[top--];
             switch (c) {
@@ -83,22 +72,16 @@ int evaluatePostfix(char* postfix) {
         }
     }
     
-    return stack[top]; // Result of the evaluation
+    return stack[top]; 
 }
 
 int main() {
     char infix[MAX], postfix[MAX];
-
-    // Input the infix expression
     printf("Enter a valid infix expression: ");
     scanf("%s", infix);
-
-    // Convert infix to postfix
     infixToPostfix(infix, postfix);
     printf("Postfix Expression: %s\n", postfix);
-
-    // Evaluate the postfix expression
-    int result = evaluatePostfix(postfix);
+i   nt  result = evaluatePostfix(postfix);
     printf("Result: %d\n", result);
 
     return 0;
